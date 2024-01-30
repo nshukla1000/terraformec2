ecda1483b164558b3b9f65111fc5541fc95f58cc
nshukla1000 <65841041+nshukla1000@users.noreply.github.com>
2024-01-29 20:44:06 -0600

    Create variables.tf

---
+++ nst/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0e9107ed11be76fde"
+}
+
+variable "instance" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
